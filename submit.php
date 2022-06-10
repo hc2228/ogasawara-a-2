4f3a63b65b9344fc490da54257e737c87b51192b
sumirekato <s2042036DW@s.chibakoudai.jp>
2022-06-10 16:52:21 +0900

    test

---
+++ submit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8' />
+    <title>検索</title>
+  </head>
+
+  <body>
+
+  <!-- 自由記述 -->
+  ■自由記述：<br>
+  <?php print($_POST["freeans"]); ?>
+
+  </body>
+
+</html>
--- test.php
+++ test.php
@@ -2,15 +2,21 @@
 <html>
   <head>
     <meta charset='utf-8' />
-    <title>switch文お試しプログラム</title>
+    <title>検索画面</title>
   </head>
   <body>
   <p>
 
-  <form method="post" action="submit-0517(2).php">
-   本を入力してください：<input type="text" name="number" /><br><br>
-   <input type="submit" value="送信" />　
-   <input type="reset" value="取り消し" />
+  <form method="post" action="submit.php">
+  <br><br>
+      <textarea name="freeans" placeholder="自由に意見を記述してください" rows="4" cols="40"></textarea>
+    <!--
+    テキストボックスのプログラムをここに記載する
+    パラｰメータ：name="freeans" rows="4" cols="40" 
+    -->
+    <br><br>
+    <input type="submit" value="送信" />　
+    <input type="reset" value="取り消し" />
   </form>
 
   </p>
