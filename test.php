dac4d0b44e44ce83aa8ca154f3061dec57bc0619
sumirekato <s2042036DW@s.chibakoudai.jp>
2022-06-10 16:44:50 +0900

    test

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8' />
+    <title>switch文お試しプログラム</title>
+  </head>
+  <body>
+  <p>
+
+  <form method="post" action="submit-0517(2).php">
+   本を入力してください：<input type="text" name="number" /><br><br>
+   <input type="submit" value="送信" />　
+   <input type="reset" value="取り消し" />
+  </form>
+
+  </p>
+  </body>
+</html>
