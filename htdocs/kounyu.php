3487349a11047c9ec524b140eb023763f9c9bdf5
自分の名前 <>
2022-07-01 17:35:09 +0900

    a

--- htdocs/kounyu.php
+++ htdocs/kounyu.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <title>研究室書籍管理システム検索画面</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="kounyu.css">
     </head>
     
